e22f77e0954a04f0844604c1017c663ecececa4d
Nicoly <nicolyvalferreira.564@gmail.com>
2023-08-07 14:20:29 -0300

    feat: show posts contents in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,19 @@
+
 const posts = [];
+
 function savePost() {
     const title = document.getElementById("title").value;
     const resume = document.getElementById("resume").value;
     const publisher = document.getElementById("publisher").value;
     const date = document.getElementById("date").value;
 
+    console.log(title, resume, publisher, date);
+
     if (title && resume && publisher && date) {
         storePost(title, resume, publisher, date);
     }
 }
+
 function storePost(title, resume, publisher, date) {
     const post = {
         title,
@@ -17,4 +22,25 @@ function storePost(title, resume, publisher, date) {
         date
     };
     posts.push(post);
+    showPosts();
+
+}
+
+function showPosts() {
+    let showContent = "";
+    posts.forEach((post, index) => {
+        showContent += `
+    <div class="post">
+        <h2>${post.title}</h2>
+        <p><strong>Resumo:</strong>${post.resume}</p>
+        <p><strong>Autor:</strong>${post.publisher}</p>
+        <p><strong>Data de publicação</strong>${post.date}</p>
+
+        <button onclick="editPost(${index})">Editar</button>
+        <button onclick="removePost(${index})">Remover</button>
+    </div>
+        `;
+    })
+
+    document.getElementById("list").innerHTML = showContent;
 }
