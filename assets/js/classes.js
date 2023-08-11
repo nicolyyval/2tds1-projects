1dcbda4a59e8c44f63162270c3d9641a299a9787
Nicoly <nicolyvalferreira.564@gmail.com>
2023-08-11 13:45:59 -0300

    feat: finish pizza site

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -13,3 +13,68 @@ user1.introduce();
 
 const user2 = new User('Nicoly', 16);
 user2.introduce();
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Pizza {
+    constructor(base, sauce, toppings, cheese,) {
+        this.base = base;
+        this.sauce = sauce;
+        this.toppings = toppings;
+        this.cheese = cheese;
+    }
+    getDesc() {
+        return `Uma deliciosa pizza ${this.base}, 
+        com molho ${this.sauce}, com cobertura ${this.toppings}
+        e com o queijo ${this.cheese}`
+    }
+    getPriceBase() {
+        if (this.base == 'traditional') {
+            return 5;
+        } else if (this.base == 'integral') {
+            return 7;
+        } else {
+            return `Não trabalhamos com essa base: ${this.base}`;
+        }
+    }
+    getPriceSauce() {
+        if (this.sauce == 'tomato') {
+            return 3;
+        } else if (this.sauce == 'pesto') {
+            return 6;
+        } else {
+            return `Não trabalhamos com esse molho: ${this.sauce}`;
+        }
+    }
+    getPriceToppings() {
+        if (this.toppings == 'pepperoni') {
+            return 10;
+        } else if (this.toppings == 'chicken') {
+            return 9;
+        } else {
+            return `Não trabalhamos com essa cobertura: ${this.toppings}`;
+        }
+    }
+    getPriceCheese() {
+        if (this.cheese == 'cheedar') {
+            return 20;
+        } else if (this.cheese == 'mozzarella') {
+            return 7;
+        } else {
+            return `Não trabalhamos com esse queijo: ${this.cheese}`;
+        }
+    }
+    getTotalPricePizza() {
+        return (this.getPriceBase() + this.getPriceSauce() + this.getPriceToppings() + this.getPriceCheese());
+    }
+}
+
+const pizza1 = new Pizza('traditional', 'tomato', 'pepperoni', 'cheedar');
+const pizza2 = new Pizza('integral', 'pesto', 'chicken', 'cheedar', 'traditional');
+const pizza3 = new Pizza('')
+console.log(pizza1);
+console.log(pizza1.getDesc());
+console.log(pizza1.getTotalPricePizza());
+console.log(pizza2.getDesc());
+console.log(pizza2.getTotalPricePizza());
+console.log(pizza3.getDesc());
+console.log(pizza3.getTotalPricePizza());
