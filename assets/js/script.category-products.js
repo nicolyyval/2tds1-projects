3216d96aa5b448ff1a05961b0fb923cac5e12c26
Nicoly <nicolyvalferreira.564@gmail.com>
2023-08-18 10:39:46 -0300

    feat: create productservice

--- assets/js/script.category-products.js
+++ assets/js/script.category-products.js
@@ -34,6 +34,22 @@ class CategoryService {
 
 }
 
+class ProductService {
+    constructor() {
+        this.products = [];
+        this.nextProductId = 1;
+    }
+
+    addProduct(name, price, category) {
+        const id = this.nextProductId;
+        this.nextProductId++;
+
+        const product = new Product(id, name, price, category);
+
+        this.products.push(product);
+    }
+}
+
 const categoriesList = new CategoryService();
 
 function createCategory() {
