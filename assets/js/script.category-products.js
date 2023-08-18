59be91b9e24bf07cc7bb78f80e4de17b34947c43
Nicoly <nicolyvalferreira.564@gmail.com>
2023-08-18 10:17:26 -0300

    feat: create classes

---
+++ assets/js/script.category-products.js
@@ -0,0 +1,41 @@
+//Cada Categoria possui vários produtos
+//Cada produto é pertencente a uma Categoria
+
+class Category {
+    constructor(id, name) {
+        this.id = id;
+        this.title = name;
+        this.products = [];
+    }
+}
+
+class Product {
+    constructor(id, name, price, category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+}
+
+class CategoryService {
+    constructor() {
+        this.categories = [];
+        this.nextCategoryId = 1;
+    }
+
+    addCategory(name) {
+        const id = this.nextCategoryId;
+        this.nextCategoryId++;
+
+        const category = new Category(id, name);
+        this.categories.push(category);
+    }
+
+}
+
+const categoriesList = new CategoryService();
+
+function createCategory() {
+
+}
